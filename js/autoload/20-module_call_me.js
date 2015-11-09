657af4f38120db6fc043918ec44595bfc56e0909
almond <almond.ddt@gmail.com>
2015-11-09 18:00:22 +0300

    init

---
+++ js/autoload/20-module_call_me.js
@@ -0,0 +1,3 @@
+(function(){ 
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = false; s.src = '/modules/callme/js/callme.js'; var ss = document.getElementsByTagName('body')[0]; ss.appendChild(s);
+})();
